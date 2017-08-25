bab9ea0e90ab57d1411a26804b04379dfda9ee99
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-08-25 16:23:41 +0700

    finish app logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,33 @@
-const test = () => {
-  console.log('Hello from app.js');
-};
+$(() => {
+  view.renderInit();
 
-test;
+  $('#title').change(() => {
+    view.renderJob();
+  });
+
+  $('#design').change(() => {
+    view.renderTshirt();
+  });
+
+  $('#payment').change(() => {
+    view.renderPayment();
+  });
+
+  $('.activities input').change(e => {
+    let checkedTarget = e.target.name.replace('-', '');
+    controller.setActivities();
+    view.renderActivities(checkedTarget);
+    view.renderBilling();
+  });
+
+  $('form').submit(e => {
+    e.preventDefault();
+    let isValid = controller.isValid();
+
+    if (isValid[0]) {
+      view.renderValidation(`Form is valid, registration successful.`);
+    } else {
+      view.renderValidation(`Form is invalid, ${isValid[1]}`);
+    }
+  });
+});
