628b33f1a9d2cd07511a3dbc14d0a7a830e5367c
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-08-25 16:23:54 +0700

    finish data/modul

---
+++ src/js/data.js
@@ -0,0 +1,21 @@
+let data = {
+  activities: {
+    all: false,
+    buildtools: false,
+    express: false,
+    jsframeworks: false,
+    jslibs: false,
+    node: false,
+    npm: false
+  },
+  price: {
+    all: 200,
+    buildtools: 100,
+    express: 100,
+    jsframeworks: 100,
+    jslibs: 100,
+    node: 100,
+    npm: 100
+  },
+  billing: 0
+};
